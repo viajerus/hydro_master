24ac14318feca414d537fb18d151b2e8dda78769
viajerus <abanto@stud.uni-heidelberg.de>
2023-11-22 13:31:29 +0100

    upd

--- EZG 23/ezg_proj.R
+++ EZG 23/ezg_proj.R
@@ -26,38 +26,32 @@ workdat<-merge(abfluss,nis,by = "date")
 
 workdat$Stunde <- NULL
 
+workdat$Wert <- gsub(",", ".", workdat$Wert)
+
+workdat$Wert <- as.numeric(workdat$Wert)
+
+workdat <- tibble::rowid_to_column(workdat, "ID")
+
+
 save(workdat, file = "/Users/dabanto/Downloads/workdat.RData")
 
 #aggreggierte tageswerte
 
-nis_sum <-
-  nis %>% 
-  tidyr::drop_na(date) %>%             
-  mutate(day = floor_date(   
-    date,
-    unit = "day")) %>% 
-  group_by(day)%>%
-  summarize(n_sum = sum(N))  
 
-
-
-ggp <- ggplot(nis_sum)  + 
-  geom_bar(aes(x=day, y=n_sum),stat="identity", fill="cyan",colour="#006000")+
-  scale_x_date(date_breaks = "24 weeks",
-               date_labels = "%b-%y") + 
-    labs(title= "Aggregierter Niederschlag (mm)",
-       x="Jahr-Monat",y="Niederschlagssumme")+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size=12, hjust = 0.5),
-        axis.title.y = element_text(color = "orange", size=10, hjust = 0.5, vjust = 0.6))
-ggp
+#plot niederschlag und 
+par(mfrow=c(2,1),mar=c(0,4,4,2))
+plot(workdat$date,workdat$Wert,type="l",ylab="Abfluss [m3/s]",xlab="Datum",main="Stundenmittelwerte (Kinzig Schenkenzell)")
+par(mar=c(4,4,0,2))
+plot(workdat$date,workdat$N,type="l",col="darkblue",ylab="Niederschlag [mm]",xlab="Datum")
 
 
 #nach events suchen
 events.P = eventPOT(workdat$N, threshold = 15, min.diff = 1)
 
+#plot events
 plotEvents(workdat$N, dates = workdat$date, events = events.P, type = "hyet", colline = "#377EB8", colpnt = "#E41A1C",ylab = "Rainfall (mm)", xlab = 2015, main = "")
 
-workdat <- tibble::rowid_to_column(workdat, "ID")
+
 
 #rows extrahieren
 
@@ -81,9 +75,6 @@ p1
 
 str(new)
 
-new$Wert <- gsub(",", ".", new$Wert)
-
-new$Wert <- as.numeric(new$Wert)
 
 new$Wert <- new$Wert*1000*3600/62805000  #mm/hour
 
@@ -110,10 +101,6 @@ df <- limbs(data = new_test$Wert, dates = d, events = BFI_res)
 
 
 
-
-
-
-
 #iterate over every event to calculate baseflow
 
 result_data <- data.frame()
@@ -136,13 +123,13 @@ result_data <- result_data %>%
   drop_na()
 
   
- agg <- result_data %>%
+agg <- result_data %>%
   group_by(event_id) %>%
   summarise(across(c(Wert, N, baseflow), sum))
 
 agg <- agg %>% 
-  rename(Qges = Wert) %>% 
-  rename(Qb = baseflow)
+  dplyr::rename(Qges = Wert) %>% 
+  dplyr::rename(Qb = baseflow)
 
 
 agg$Qf <- (agg$Qges-agg$Qb)
@@ -156,18 +143,40 @@ for (i in 1:nrow(agg)) {
     select(Wert, N) %>% 
     mutate(id = row_number())
   
-  BFI_res <- tryCatch(eventBaseflow(new_t$Wert), error = function(e) NA)
+  BFI_res <- tryCatch(eventBaseflow(new_t$Wert), error = function(e) return(NA))
   print(rep(i))
-  ifelse(!is.na(BFI_res), df <- limbs(data = new_test$Wert, dates = NULL, events = BFI_res), NA)
+  ifelse(!is.na(BFI_res), df <- limbs(data = new_test$Wert, 
+                                      dates = NULL, events = BFI_res), NA)
   
   agg$Q_event[i] <- ifelse(is.na(df), NA, df[1, "sum"])
   agg$srt_event[i] <- ifelse(is.na(df), NA, df[1, "srt"])
   agg$end_event[i] <- ifelse(is.na(df), NA, df[1, "end"])
   
+  agg$sum_n[i] <- new %>%
+    filter(Name == i) %>% 
+    slice(agg$srt_event[i]:agg$end_event[i]) %>%
+    summarise(sum_value = sum(N)) %>% 
+    pull(sum_value)
+  
 }
 
+agg$srt_event[3]
 
-agg$RFC_2 <- agg$Q_event/agg$N
+
+
+
+result <- new %>%
+  filter(Name == 1) %>% 
+  slice(agg$srt_event[1]:agg$end_event[1]) %>%
+  summarise(sum_value = sum(N)) %>% 
+  pull(sum_value)
+
+
+
+
+
+
+agg$RFC_2 <- agg$Q_event/agg$sum_n
 
 
 plot_q <- agg %>% 
@@ -177,13 +186,10 @@ plot_q <- as.data.frame(plot_q)
 
 plot_q <- melt(plot_q, id=c("event_id"))
 
-
 plot_q %>% 
   group_by(variable) %>% 
   ggplot(aes(x = event_id, y = value, group = variable, color = variable)) +
-  geom_point() +
-  geom_line() +
-  facet_wrap(~variable, nrow = 2, scales = "free")
+  geom_line() 
 
 
 
@@ -202,3 +208,32 @@ ggplot(df, aes(x = x, y = y, color = variable2, fill = variable2)) +
   geom_line(aes(group = variable2), size = 1.5, color = "red") +
   facet_wrap(~variable1, scales = "free_y") +
   labs(title = "Facet Wrap Plot with Bar Plot and Line")
+
+
+
+
+
+
+
+
+ggp_2 <- ggplot(plot_q)  + 
+  geom_line(aes(x=event_id, y=RFC),stat="identity", fill="cyan",colour="#006000")+
+  geom_line(aes(x=event_id, y=1000*RFC_2),stat="identity",color="red",size=0.5)+
+  scale_x_date(date_breaks = "8 weeks",
+               date_labels = "%b-%y") + 
+  
+  labs(title= "Geolocated COVID-19 related tweets for all of BW and mean \n  average sentiment score trend (weekly)",
+       x="Year-Month",y="Geolocated COVID-19 tweets")+
+  scale_y_continuous(sec.axis=sec_axis(~.*0.001,name="Average sentiment score")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size=11, hjust = 0.5),
+        axis.title.y = element_text(color = temperatureColor, size=10, hjust = 0.5, vjust = 0.6),
+        axis.title.y.right = element_text(color = priceColor, size=10, hjust = 0.5, vjust = 0.6))
+ggp_2
+
+
+
+
+
+
+
+
